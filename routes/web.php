cfe0ee0c5c3c0c865b7fd6644b479e9bc11e52fd
Rustam Timalsina <rustamtimalsina179@gmail.com>
2026-09-16 00:08:38 +0545

    Improve authentication routes

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
-Route::get('/dashboard', function () {
-    return view('dashboard');
+
+Route::get('/', function () {
+    return redirect()->route('login');
 });
+
 Route::get('/login', [LoginController::class, 'showLoginForm'])
     ->name('login');
 
 Route::post('/login', [LoginController::class, 'login'])
     ->name('login.process');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware('auth')
     ->name('dashboard');
+
 Route::post('/logout', [LoginController::class, 'logout'])
     ->middleware('auth')
     ->name('logout');
